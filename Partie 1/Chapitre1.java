d2b5d2ddb17113940e2b448d6cefa80984d871b3
Ramzi Mejri <kingrxm82@gmail.com>
2024-05-22 12:17:17 +0100

    Chapitre 1

---
+++ Partie 1/Chapitre1.java
@@ -0,0 +1,34 @@
+public class Chapitre1 {
+    public static void main(String[] args) {
+        //Déclaration des variable
+        int epargne = 500; //Entier
+        double moyenne = 14.2;//Float
+        String hello = "Hello World";//String
+
+        //Les opérations
+        epargne += 100;
+        moyenne /= 2;
+        hello = "Hello Word !";
+
+        //Afficher les variables
+        System.out.println(epargne);
+        System.out.println(moyenne);
+        System.out.println(hello);
+
+
+        // //Variables ne changera jamais
+        // final int epargnex = 500;
+        // final double moyennex = 14.2;
+        // final String hellox = "Hello World";
+
+        // //Les opérations sur ces derniers variables
+        // epargnex += 100;
+        // moyennex /= 2;
+        // hellox = "Hello Word !";
+
+        // //Afficher les variables
+        // System.out.println(epargnex);
+        // System.out.println(moyenne);
+        // System.out.println(hellox);
+    }
+}
